4d27bbe75b8eb19aec491b751ba3e0f97539fdc8
ShahiAmit <100249381+ShahiAmit@users.noreply.github.com>
2022-04-02 23:41:46 +0530

    Add files via upload

---
+++ March22 Web Fundamentals Assignments/MARCH/Week4/Assignment14/prog2.js
@@ -0,0 +1,14 @@
+function MaxNo(){
+    var a=10,b=15,c=20;
+    if(a>b && a>c)
+    {
+        console.log("A is max or highest no");
+    }
+    if(b>a && b>c)
+    {
+        console.log("B is max or highest no");
+    }
+    else{
+        console.log("C is max or highest no");
+    }
+}
